603a425c3c0148138e5d6aac06abf579e76cd32b
schultkr <schult.christoph@googlemail.com>
2016-05-17 18:20:19 +0200

    add matalb Q #8 by CS

--- QID-2386-BaseCorrGaussModelCDO/BaseCorrGaussModelCDO.R
+++ QID-2386-BaseCorrGaussModelCDO/BaseCorrGaussModelCDO.R
@@ -79,3 +79,18 @@ BaseCorrGaussModelCDO = function(a, R, defProb, UAP, LAP, DF, DayCount, trueSpre
     y = abs(spread - trueSpread)
     return(y)
 }
+
+# Example Call
+
+# define input parameters
+a          = 0.5;
+trueSpread = 20.45;
+recoveryR  = 0.4;
+UAP        = 0.03;
+LAP        = 0;
+DF         = 0.9;
+DayCount   = 0.25;
+defProb    = 0.001;
+
+# execute function
+BaseCorrGaussModelCDO(a, recoveryR, defProb, UAP, LAP, DF, DayCount, trueSpread)
